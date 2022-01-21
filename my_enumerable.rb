750a0c64f1e3fbeccf65d5898c82f0482ec1afc7
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-01-21 11:36:35 -0400

    Add methods my_all? and  my_any?

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,10 +1,33 @@
-# frozen_string_literal: true
-
-# MyEnumerable module
 module MyEnumerable
+  # MyAll module
+  def my_all?
+    return unless block_given?
+
+    response = false
+    @list.each do |n|
+      response = true if yield(n)
+    end
+    response
+  end
+
+  # MyAny module
+  def my_any?
+    return unless block_given?
+
+    any = false
+    @list.each do |n|
+      any = true if yield(n)
+    end
+    any
+  end
+
+  # MyEnumerable module
+
   def filter
     filtered = []
-    each { |item| filtered.push(item) if yield item }
+    @list.each do |item|
+      filtered.push(item) if yield(item)
+    end
     filtered
   end
 end
